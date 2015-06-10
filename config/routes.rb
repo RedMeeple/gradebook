fe708ecd46d563c4950e32334c40c82696fb08e3
Anna Bedell <awbedell@gmail.com>
2015-06-10 13:05:39 -0400

    scaffolds built

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :assignments
+  resources :grades
+  resources :parents
+  resources :students
+  resources :teachers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
