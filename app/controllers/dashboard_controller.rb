1fa36db4e4880d868a504532b8a9a01b8ceb5588
Anna Bedell <awbedell@gmail.com>
2015-06-10 13:27:30 -0400

    different login sessions for student, teacher and parent

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,46 @@
+class DashboardController < ApplicationController
+  before_action :instructor_logged_in?, only: [:instructor]
+  before_action :student_logged_in?, only: [:student]
+  before_action :parent_logged_in?, only: [:parent]
+
+  def index
+    if session[:user_type] == "teacher"
+      redirect_to :controller => 'dashboard', :action => 'teacher'
+    elsif session[:user_type] == "student"
+      redirect_to :controller => 'dashboard', :action => 'student'
+    elsif session[:user_type] == "parent"
+      redirect_to :controller => 'dashboard', :action => 'parent'
+    else
+      redirect_to sessions_login_path
+  end
+
+  def student
+    @student = Student.find_by_id(session[:user_id])
+  end
+
+  def parent
+    @parent = Parent.find_by_id(session[:user_id])
+  end
+
+  def teacher
+    @teacher = Teacher.find_by_id(session[:user_id])
+  end
+
+  private def instructor_logged_in?
+    unless Teacher.find_by_id(session[:user_id]) && (session[:user_type] == "teacher")
+      redirect_to sessions_login_path, notice: 'User or Password does not match our records.'
+    end
+  end
+
+  private def student_logged_in?
+    unless Student.find_by_id(session[:user_id]) && (session[:user_type] == "student")
+      redirect_to sessions_login_path, notice: 'User or Password does not match our records.'
+    end
+  end
+
+  private def parent_logged_in?
+    unless Parent.find_by_id(session[:user_id]) && (session[:user_type] == "parent")
+      redirect_to sessions_login_path, notice: 'User or Password does not match our records.'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
+  get 'dashboard/student'
+
+  get 'dashboard/parent'
+
+  get 'dashboard/teacher'
+
+  get 'sessions/login'
+
+  get 'sessions/logout'
+
   resources :assignments
   resources :grades
   resources :parents
   resources :students
   resources :teachers
+
+  get 'dashboard/index'
+  root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
